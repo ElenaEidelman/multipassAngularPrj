cf5b9b3a64036282bcc7e1e9d58f0ba41b0b831f
Alena <marlen8088@gmail.com>
2021-01-26 22:18:24 +0200

    updating 1

--- src/app/exec-order/exec-order.component.spec.ts
+++ src/app/exec-order/exec-order.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ExecOrderComponent } from './exec-order.component';
 
@@ -6,7 +6,7 @@ describe('ExecOrderComponent', () => {
   let component: ExecOrderComponent;
   let fixture: ComponentFixture<ExecOrderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ExecOrderComponent ]
     })
